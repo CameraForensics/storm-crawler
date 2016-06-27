daf5c7d23e086cda4c05dd412d35260bad464510
Julien Nioche <julien@digitalpebble.com>
2016-06-27 10:39:16 +0100

    Initial commit jBrowserDriver

---
+++ core/src/main/java/com/digitalpebble/storm/crawler/protocol/jbrowser/HttpProtocol.java
@@ -0,0 +1,91 @@
+/**
+ * Licensed to DigitalPebble Ltd under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * DigitalPebble licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.digitalpebble.storm.crawler.protocol.jbrowser;
+
+import org.apache.storm.Config;
+import org.slf4j.LoggerFactory;
+
+import com.digitalpebble.storm.crawler.Metadata;
+import com.digitalpebble.storm.crawler.protocol.AbstractHttpProtocol;
+import com.digitalpebble.storm.crawler.protocol.ProtocolResponse;
+import com.digitalpebble.storm.crawler.util.ConfUtils;
+import com.machinepublishers.jbrowserdriver.JBrowserDriver;
+import com.machinepublishers.jbrowserdriver.Settings;
+
+import crawlercommons.robots.BaseRobotRules;
+
+/**
+ * Uses JBrowserdriver to handle http and https
+ **/
+
+public class HttpProtocol extends AbstractHttpProtocol {
+
+    private static final org.slf4j.Logger LOG = LoggerFactory
+            .getLogger(HttpProtocol.class);
+
+    private JBrowserDriver driver = null;
+
+    @Override
+    public void configure(Config conf) {
+        super.configure(conf);
+
+        // TODO configure timeout + http agent +proxy etc...
+        Settings settings = Settings.builder().headless(true).build();
+
+        driver = new JBrowserDriver(settings);
+    }
+
+    public ProtocolResponse getProtocolOutput(String url, Metadata metadata)
+            throws Exception {
+
+        // This will block for the page load and any
+        // associated AJAX requests
+        driver.get(url);
+
+        // TODO charset
+        byte[] content = driver.getPageSource().getBytes();
+
+        // TODO add http response to metadata
+        return new ProtocolResponse(content, driver.getStatusCode(), metadata);
+    }
+
+    public static void main(String args[]) throws Exception {
+        HttpProtocol protocol = new HttpProtocol();
+        Config conf = new Config();
+
+        String url = args[0];
+
+        ConfUtils.loadConf(args[1], conf);
+        protocol.configure(conf);
+
+        if (!protocol.skipRobots) {
+            BaseRobotRules rules = protocol.getRobotRules(url);
+            System.out.println("is allowed : " + rules.isAllowed(url));
+        }
+
+        Metadata md = new Metadata();
+        ProtocolResponse response = protocol.getProtocolOutput(url, md);
+        System.out.println(url);
+        System.out.println(response.getMetadata());
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getContent().length);
+
+        System.exit(0);
+    }
+
+}
